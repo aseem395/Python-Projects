7ec68d4a8464ae7c6a703662a1c438a884a94c5e
aseem395 <aseemajit@gmail.com>
2025-05-10 11:36:12 +0530

    Update Shoutouts.py

--- Shoutouts.py
+++ Shoutouts.py
@@ -1,5 +1,7 @@
+# We have created a program to give shoutouts based on the input list given by the user.
 import win32com.client as wincom
-l = ["Rahul", "Nishant", "Harry"]
+input = input("Enter the name of the person: ")
+l = input.split()
 speak = wincom.Dispatch("SAPI.SpVoice")
 for people in l:
     text = f"Shoutout to {people}"
